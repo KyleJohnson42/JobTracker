a3bb072d427eb7495ec794425f1b582524f95209
Kyle Johnson <zion4to6@gmail.com>
2021-03-30 10:15:56 -0700

    App skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const App = () => (
-  <div>Hello, world!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: []
+    }
+
+    this.getAllJobs = this.getAllJobs.bind(this);
+    this.updateMetrics = this.updateMetrics.bind(this);
+  }
+
+  getAllJobs(callback = () => {}) {
+
+  }
+
+  updateMetrics() {
+
+  }
+
+  componentDidMount() {
+    this.getAllJobs(this.updateMetrics);
+  }
+
+  render() {
+    return (
+      <div>Hello, world!</div>
+    )
+  }
+}
 
 export default App;
