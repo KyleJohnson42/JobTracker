d03d913f63efd18829bc334b1ab8f6c87536a432
Kyle Johnson <zion4to6@gmail.com>
2021-03-30 10:33:43 -0700

    Database setup

--- database/index.js
+++ database/index.js
@@ -1,1 +1,24 @@
-module.exports = {};
+const mongoose = require('mongoose');
+mongoose.connect(`${process.env.DB_URL}`);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  const jobSchema = new mongoose.Schema({
+    title: String,
+    company: String,
+    link: String,
+    notes: String,
+    applied: Boolean,
+    phone: Boolean,
+    interview: Boolean,
+    offer: Boolean
+  });
+
+  const Job = mongoose.model('Job', jobSchema);
+
+});
+
+module.exports = {
+  db: db
+}
