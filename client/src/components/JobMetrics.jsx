f7bfa01110c74c1c5e8887bf9cec25e3c4a99fef
Kyle Johnson <zion4to6@gmail.com>
2021-03-31 10:57:46 -0700

    Fixed percentage displays

--- client/src/components/JobMetrics.jsx
+++ client/src/components/JobMetrics.jsx
@@ -8,11 +8,11 @@ const JobMetrics = ({ jobs, jobsNotYetApplied, jobsApplied, jobsPhone, jobsInter
       <span>{`${jobsNotYetApplied} are awaiting an application,`}</span>
       <br />
       <span>{`${jobsApplied} have been applied to,`}</span>
-      <span className="conversion">{`${(jobsPhone / jobsApplied * 100).toFixed(0)}%`}</span>
+      <span className="conversion">{`${(jobsPhone / jobsApplied * 100 || 0).toFixed(0)}%`}</span>
       <span>{`${jobsPhone} have resulted in a request for a phone screen,`}</span>
-      <span className="conversion">{`${(jobsInterview / jobsPhone * 100).toFixed(0)}%`}</span>
+      <span className="conversion">{`${(jobsInterview / jobsPhone * 100 || 0).toFixed(0)}%`}</span>
       <span>{`${jobsInterview} have resulted in a request for an interview,`}</span>
-      <span className="conversion">{`${(jobsOffer / jobsInterview * 100).toFixed(0)}%`}</span>
+      <span className="conversion">{`${(jobsOffer / jobsInterview * 100 || 0).toFixed(0)}%`}</span>
       <span>{`${jobsOffer} have resulted in offers`}</span>
     </div>
     <br />
